a43b52f16b14516788dfb234ab83eb6f65152caf
samuel555-06 <samuelwarframe1@gmail.com>
2026-09-11 15:36:40 -0500

    Agregar CSRF_TRUSTED_ORIGINS para producción

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
-
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
 
 # Application definition
 
